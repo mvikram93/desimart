95176bbf03a0b8e441f62e1904737da1519c9caa
yagnasri100 <yagna.madepally@gmail.com>
2024-03-12 01:17:39 -0400

    Added resources folder and changed loginscreen interface

--- view/LoginScreen.py
+++ view/LoginScreen.py
@@ -1,13 +1,15 @@
 import tkinter as tk
+from tkinter import *
 import logging
 from controller.LoginController import LoginController as Login
 from view.ChangePasswordScreen import ChangePasswordScreen
+from view.RegistrationScreen import RegistrationScreen
+
 
 from model.UserModel import User
 
 logging.basicConfig(level=logging.INFO, filename='app.log', filemode='w',
                     format='%(name)s - %(levelname)s - %(message)s')
-
 
 class LoginScreen(tk.Toplevel):
     def __init__(self):
@@ -16,24 +18,48 @@ class LoginScreen(tk.Toplevel):
         log = logging.getLogger(self.__class__.__name__)
         log.info("Opened Login Screen")
         self.title("Login Screen")
+        #changing the icon of the window
+        self.iconbitmap("resources/user1.ico")
+        self.configure(bg='slate gray')
+        #sets the overall size of the main window
+        self.geometry("750x500")
+       
         self.user = User()
-        self.login_frame = tk.Frame(self)
-        self.login_frame.pack()
+        self.login_frame = tk.Frame(self, bg='slate gray')
+        self.login_frame.place(relx=0,rely=0)
 
-        self.email_label = tk.Label(self.login_frame, text="E-Mail:")
-        self.email_label.pack()
+        self.header_label=tk.Label(self, text="LOGIN!",bg='slate gray',font=("Segoe Script", 20, "bold"),fg='Black')
+        self.header_label.place(relx=0.45, rely=0.1)
 
-        self.email_entry = tk.Entry(self.login_frame)
-        self.email_entry.pack()
+        self.email_label = tk.Label(self, text="E-Mail:",bg='slate gray',font=8,fg='Black')
+        self.email_label.place(relx=0.2, rely=0.3)
 
-        self.password_label = tk.Label(self.login_frame, text="Password:")
-        self.password_label.pack()
+        self.email_entry = tk.Entry(self,font=8,fg='Black')
+        self.email_entry.place(relx=0.4, rely=0.3)
 
-        self.password_entry = tk.Entry(self.login_frame)
-        self.password_entry.pack()
+        self.password_label = tk.Label(self, text="Password:",bg='slate gray',font=8,fg='Black')
+        self.password_label.place(relx=0.15, rely=0.4)
 
-        self.submit_button = tk.Button(self.login_frame, text="Submit", command=self.submit_form)
-        self.submit_button.pack()
+        self.password_entry = tk.Entry(self, font=8,fg='Black',show='*')
+        self.password_entry.place(relx=0.4, rely=0.4)
+
+        #using submit image as button
+        self.submit_image = PhotoImage(file="resources/submit_button.png")
+        self.resized_image=self.submit_image.subsample(5,5)
+        self.submit_button = tk.Button(self, image=self.resized_image, command=self.submit_form, bg='slate gray', bd=0)
+        self.submit_button.place(relx=0.7, rely=0.5)
+
+        # creating register button
+        self.register_image = PhotoImage(file="resources/register_button.png")
+        self.re_register_image=self.register_image.subsample(5,5)
+        self.greet_button = tk.Button(self, image=self.re_register_image, command=self.open_registration_screen,bg='slate gray',bd=0)
+        self.greet_button.place(relx=0.2,rely=0.5)
+
+        # Add a exit button
+        self.exit_image = PhotoImage(file="resources/power_button.png")
+        self.re_exit_image=self.exit_image.subsample(10,10)
+        self.exit_button = tk.Button(self, image=self.re_exit_image, command=self.destroy,bg='slate gray',bd=0)
+        self.exit_button.place(relx=0.5,rely=0.5)
 
     def submit_form(self):
         login = Login()
@@ -44,3 +70,10 @@ class LoginScreen(tk.Toplevel):
             self.withdraw()
             changePassword = ChangePasswordScreen(self.user)
             #changePassword.change_password()
+
+    def open_registration_screen(self):
+        self.withdraw()
+        self.reg = RegistrationScreen()
+   
+
+
